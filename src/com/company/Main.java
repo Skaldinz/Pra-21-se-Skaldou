92eb2424ded3452acded9bd2d5e995e03e8a507e
michal.skalicky <michalskalda@gmail.com>
2021-09-21 14:43:14 +0200

    Zajda neda klik

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Ahoj svete");
         System.out.println("Zelena je tráva, zajda neda klik");
-
+        System.out.println("Karbanova máma, má chuť na piknik");
     }
 }
